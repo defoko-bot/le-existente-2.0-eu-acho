1da662675b859bd7acb38eb728fa32a23f7b8eb9
chocoboii~ <63813281+defoko-bot@users.noreply.github.com>
2020-06-09 18:24:50 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,7 +24,7 @@ bot.on("message", (msg) => {
         case "sabiaspalavras": //fala um comando foda ae to sem ideia
             const wrdrandom = sabio[Math.floor(Math.random() * sabio.length)]
             console.log(`Sabia palavra invocada: ${wrdrandom}, pos na lista: ${sabio.indexOf(wrdrandom)}`)
-            const wrdembed = new msgEmbed()
+            const wrdembed = new MessageEmbed()
             .setTitle(`Sua palavra sagrada éééé...`)
             .setDescription(`${wrdrandom}`);
             msg.channel.send(wrdembed)
@@ -44,7 +44,7 @@ bot.on("message", (msg) => {
             if(numerorandomae == 1) moedaa = "Cara";
             if(numerorandomae == 2) moedaa = "Coroa";
             console.log(`Monedita ${numerorandomae}`)
-            const flipcoin = new msgEmbed()
+            const flipcoin = new MessageEmbed()
             .setTitle(`Monetita ao ar deu...`)
             .attachFiles('./assets/moedita.gif')
             .setImage('attachment://monedita.gif')
